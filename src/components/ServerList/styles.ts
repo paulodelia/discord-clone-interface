ff9caed72c8261894f9e2e54ac1e45d805a8e226
paulodelia <paulohdelia@gmail.com>
2020-06-19 23:30:22 -0300

    feat: add ServerList Container

---
+++ src/components/ServerList/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  grid-area: SL;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background-color: var(--tertiary);
+
+  padding: 11px 0;
+
+  max-height: 100vh;
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
