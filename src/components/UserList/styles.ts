b4ecc03226366a944f81c610ae633ceda4054a02
paulodelia <paulohdelia@gmail.com>
2020-06-20 18:05:47 -0300

    feat: add UserList Container

--- src/components/UserList/styles.ts
+++ src/components/UserList/styles.ts
@@ -1,7 +1,30 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    grid-area: UL;
 
+    display: flex;
+    flex-direction: column;
+
+    padding: 3px 6px 0 16px;
+    background-color: var(--secondary);
+
+    height: 100%;
+
+    overflow-y: scroll;
+
+    ::-webkit-scrollbar {
+        width: 4px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background-color: var(--tertiary);
+        border-radius: 4px;
+    }
+
+    ::-webkit-scrollbar-track {
+        background-color: var(--secondary);
+    }
 `;
 
 export const Role = styled.div`
