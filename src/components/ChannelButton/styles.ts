60ad093ec26ba950e088e19260c9bd061f3ebe28
paulodelia <paulohdelia@gmail.com>
2020-06-20 14:25:56 -0300

    feat: add ChannelButton component

---
+++ src/components/ChannelButton/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+`;
+
+export const HashtagIcon = styled.div`
+
+`;
+
+export const InviteIcon = styled.div`
+
+`;
+
+export const SettingsIcon = styled.div`
+
+`;
