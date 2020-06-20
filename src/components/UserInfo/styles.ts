3703cfd0519ae2ffc849752ae5085c5b155713bd
paulodelia <paulohdelia@gmail.com>
2020-06-20 15:31:22 -0300

    feat: add UserInfo Container

--- src/components/UserInfo/styles.ts
+++ src/components/UserInfo/styles.ts
@@ -2,7 +2,18 @@ import styled from 'styled-components';
 import { Mic, Headset, Settings } from 'styled-icons/material';
 
 export const Container = styled.div`
+    grid-area: UI;
+    
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
+    padding: 10px;
+    
+    background-color: var(--quaternary);
+
+    box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 0px 0px;
+    z-index: 2;
 `;
 
 export const Profile = styled.div`
