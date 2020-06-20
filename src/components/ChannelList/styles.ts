643be33dce6ed51720b63963d74d80d645258e64
paulodelia <paulohdelia@gmail.com>
2020-06-20 14:12:35 -0300

    feat: add ChannelList Container

--- src/components/ChannelList/styles.ts
+++ src/components/ChannelList/styles.ts
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
+import { Add } from 'styled-icons/material';
 
 export const Container = styled.div`
+    grid-area: CL;
     
+    display: flex;
+    flex-direction: column;
+
+    padding: 24px 9.5px 0 16px;
+    background-color: var(--secondary);
 `;
 
 export const Category = styled.div`
 `;
 
-export const AddCategoryIcon = styled.div`
+export const AddCategoryIcon = styled(Add)`
 `;
