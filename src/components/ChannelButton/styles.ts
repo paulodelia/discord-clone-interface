07331468d242c182e253e8c14e533362b2f3959f
paulodelia <paulohdelia@gmail.com>
2020-06-20 14:39:31 -0300

    feat: add ChannelButton Container

--- src/components/ChannelButton/styles.ts
+++ src/components/ChannelButton/styles.ts
@@ -1,7 +1,36 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
+    padding: 5px 3px;
+    border-radius: 5px;
+
+    background-color: transparent;
+
+    transition: background-color .2s;
+
+    > div {
+        display: flex;
+        align-items: center;
+
+        > span {
+            margin-left: 5px;
+            color: var(--senary);
+        }
+    }
+
+    &.active, &:hover {
+        background-color: var(--quinary);
+
+        > div span {
+            color: var(--white);
+        }
+    }
+
+    cursor: pointer;
 `;
 
 export const HashtagIcon = styled.div`
