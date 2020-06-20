a81736f011eda8ef764e195e9c0cc27f8e0cc013
paulodelia <paulohdelia@gmail.com>
2020-06-20 14:00:15 -0300

    feat: add ChannelList component

---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Container, Category, AddCategoryIcon } from './styles';
+
+const ChannelList: React.FC = () => {
+    return (
+        <Container>
+            <Category>
+                <span>Canais de texto</span>
+                <AddCategoryIcon />
+            </Category>
+
+            {/* <ChannelButton channelName="chat-livre" />
+            <ChannelButton channelName="faculdade" />
+            <ChannelButton channelName="csgo" />
+            <ChannelButton channelName="mine" /> */}
+        </Container>
+    );
+}
+
+export default ChannelList;
---
+++ src/components/ChannelList/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    
+`;
+
+export const Category = styled.div`
+`;
+
+export const AddCategoryIcon = styled.div`
+`;
