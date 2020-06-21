3b93b5d7f5ee3de392148463ae6dda1fd07bf5a4
paulodelia <paulohdelia@gmail.com>
2020-06-20 21:01:20 -0300

    feat: add ChannelData Messages

--- src/components/ChannelData/styles.ts
+++ src/components/ChannelData/styles.ts
@@ -12,7 +12,26 @@ export const Container = styled.div`
 `;
 
 export const Messages = styled.div`
+    padding: 20px 0;
 
+    display: flex;
+    flex-direction: column;
+
+    max-height: calc(100vh - 46px - 68px);
+
+    overflow-y: scroll;
+    ::-webkit-scrollbar {
+        width: 4px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background-color: var(--tertiary);
+        border-radius: 4px;
+    }
+
+    ::-webkit-scrollbar-track {
+        background-color: var(--secondary);
+    }
 `;
 
 export const InputWrapper = styled.div`
