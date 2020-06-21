afc737f803f9392b20ea5dd4417daf0848d7b076
paulodelia <paulohdelia@gmail.com>
2020-06-20 22:01:04 -0300

    feat: go to the recents messages when page load

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
 import ChannelMessage, { Mention } from '../ChannelMessage';
 
 const ChannelData: React.FC = () => {
+    const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+    useEffect(() => {
+        const div = messagesRef.current;
+
+        if(div) {
+            div.scrollTop = div.scrollHeight;
+        }
+    }, [messagesRef]);
+
     return (
         <Container>
-            <Messages>
-                <ChannelMessage 
-                    author="Paulo D'Elia"
-                    date="20/06/2020"
-                    content="React não é tão difícil que nem eu imaginava!"
-                />
+            <Messages ref={messagesRef}>
+                {Array.from(Array(15).keys()).map((n) => (
+                    <ChannelMessage 
+                        author="Paulo D'Elia"
+                        date="20/06/2020"
+                        content="React não é tão difícil que nem eu imaginava!"
+                    />
+                ))}
                 
                 <ChannelMessage 
                     author="Jhonson"
                     date="20/06/2020"
                     content={
                         <>
-                            <Mention>@Paulo D'Elia</Mention>, Bom noite!
+                            <Mention>@Paulo D'Elia</Mention>, Boa noite!
                         </>
                     }
+                    hasMention
+                    isBot
                 />
             </Messages>
                 
