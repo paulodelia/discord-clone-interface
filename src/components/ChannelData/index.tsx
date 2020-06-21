c70f570416b99e27953b0d3c631464b29a5eea3b
paulodelia <paulohdelia@gmail.com>
2020-06-20 21:37:44 -0300

    feat: add ChannelMessage Container

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
+import ChannelMessage from '../ChannelMessage';
 
 const ChannelData: React.FC = () => {
     return (
         <Container>
-            <Messages />
-
+            <Messages>
+                <ChannelMessage 
+                    author="Paulo D'Elia"
+                    date="20/06/2020"
+                    content="React não é tão difícil que nem eu imaginava!"
+                />
+                
+                {/* <ChannelMessage 
+                    author="Jhonson"
+                    date="20/06/2020"
+                    content={
+                        <>
+                            <Mention>@Paulo D'Elia</Mention>, Bom noite!
+                        </>
+                    }
+                /> */}
+            </Messages>
+                
             <InputWrapper>
                 <Input type="text" placeholder="Conversar em #chat-livre" />
                 <InputIcon />
--- src/components/ChannelMessage/styles.ts
+++ src/components/ChannelMessage/styles.ts
@@ -2,11 +2,28 @@ import styled from 'styled-components';
 
 
 export const Container = styled.div`
+    display: flex;
+    align-items: center;
 
+    padding: 4px 16px;
+    margin-right: 4px;
+
+    background-color: transparent;
+
+    &.mention {
+        background-color: var(--mention-message);
+
+        border-left: 2px solid var(--mention-detail);
+        padding-left: 14px;
+    }
+
+    & + div {
+        margin-top: 13px;
+    }
 `;
 
 export const Avatar = styled.div`
-
+    
 `;
 
 export const Message = styled.div`
@@ -20,3 +37,5 @@ export const Header = styled.div`
 export const Content = styled.div`
 
 `;
+
+// export const Mention = styled.span``;
